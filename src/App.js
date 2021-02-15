fb4e8baf0c429e6023e33cbdd18557fa965710ba
Anirudh Kanakatte <20615787+anirudhkanakatte@users.noreply.github.com>
2021-02-15 13:32:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>let us see if this works</p>
+        <p>this is another paragraph</p>
+        <p>another one</p>
+        <p>new paragraph to test polling</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
